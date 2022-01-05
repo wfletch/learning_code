550b64ac1f45dbdb264ce420bebbe8063f51efe5
Warren Fletcher <warren@fletch.band>
2022-01-05 14:20:41 +0100

    Created a terrible way to find square roots of a number

--- tour_of_go/sqrt/sqrt.go
+++ tour_of_go/sqrt/sqrt.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+func sqrt(x float64) float64 {
+	error_margin := 1e-3
+	guess := 1.0
+	for {
+		result := math.Pow(guess, 2)
+		if math.Abs(x-result) <= error_margin {
+			return guess
+		} else {
+			if result > x {
+				guess -= 0.0001
+			} else {
+				guess += 0.0001
+			}
+		}
+	}
+}
 
 func main() {
-	fmt.Println("vim-go")
+	fmt.Println(sqrt(5))
 }
