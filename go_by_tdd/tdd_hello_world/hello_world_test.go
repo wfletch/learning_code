0487d866caa08d222d7255c5e680d78a4d620a29
Warren Fletcher <warren@fletch.band>
2022-01-14 14:06:20 +0100

    Added Ability for Empty String Input. Added Tests

--- go_by_tdd/tdd_hello_world/hello_world_test.go
+++ go_by_tdd/tdd_hello_world/hello_world_test.go
@@ -3,10 +3,21 @@ package main
 import "testing"
 
 func TestHelloWorld(t *testing.T) {
-	got := Hello("Warren")
-	want := "Hello, Warren"
-
-	if got != want {
-		t.Errorf("Got %q want %q", got, want)
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+	t.Run("saying Hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("Saying 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
 }
