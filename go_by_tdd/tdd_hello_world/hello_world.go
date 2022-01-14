55d9524c29c399542182c77296654a5710eba5fa
Warren Fletcher <warren@fletch.band>
2022-01-14 14:23:55 +0100

    Minor Last Refactor for Hello World by TDD

--- go_by_tdd/tdd_hello_world/hello_world.go
+++ go_by_tdd/tdd_hello_world/hello_world.go
@@ -8,17 +8,22 @@ const french = "French"
 const spanishHelloPrefix = "Hola, "
 const frenchHelloPrefix = "Bonjour, "
 
+func getLanguagePrefix(lang string) (prefix string) {
+	switch lang {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
 func Hello(name string, lang string) string {
 	if name == "" {
 		name = "World"
 	}
-	if lang == french {
-		return frenchHelloPrefix + name
-	}
-	if lang == spanish {
-		return spanishHelloPrefix + name
-	}
-	return englishHelloPrefix + name
+	return getLanguagePrefix(lang) + name
 }
 
 func main() {
