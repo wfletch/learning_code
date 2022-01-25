f4c77657a8aabec8b65b7012ded3fc10537f39b4
Warren Fletcher <warren@fletch.band>
2022-01-26 00:45:27 +0100

    Added value method

--- python/linked_list/linked_list.py
+++ python/linked_list/linked_list.py
@@ -1,5 +1,8 @@
 class Node():
-    def __init__(self):
+    def __init__(self,value):
+        self.value = value
         self.next_node = None
         self.prev_node = None
         pass
+    def get_value(self):
+        return self.value
