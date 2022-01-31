038a82786cdb1c86b5555c49ab2b2802f850d951
Warren Fletcher <warren@fletch.band>
2022-01-31 21:00:00 +0100

    Working Dynamic Progamming Fibonacci Solution

--- rust/Hello World!/dp/fibonacci/fib.rs
+++ rust/Hello World!/dp/fibonacci/fib.rs
@@ -1,8 +1,22 @@
 fn main() {
-
+    println!("{}", fib(42));
 }
 
-fn fib(k: i32) -> i32 {
-    let fib: [i64;2] = [1,1];
-
+fn fib(k: i32) -> i64 {
+    // Space O(1)
+    // Time O(n)
+    let mut fib: [i64;2] = [1,1];
+    if k == 0 {
+        return 0;
+    }
+    else if k <= 2{
+        return 1;
+    }
+    for _i in 3..=k {
+        // TODO: Replace this with XOR to avoid temp Variable
+        let temp = fib[0];
+        fib[0] = fib[0] + fib[1];
+        fib[1] = temp;
+    }
+    return fib[0]
 }
