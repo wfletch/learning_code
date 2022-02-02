521ed317c2ceb7c0d9c6e7ba2299aa116c836643
Warren Fletcher <warren@fletch.band>
2022-02-02 20:56:58 +0100

    WIP: Topology

---
+++ python/tree/topology.py
@@ -0,0 +1,15 @@
+def topology():
+    visited = []
+    for n in adj_matric:
+
+
+
+
+
+
+if __name__ == "__main__":
+    adj_matric =[
+            [1,2],
+            [0],
+            [0,1]]
+    topology()
