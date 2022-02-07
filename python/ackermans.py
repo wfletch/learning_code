332430cdcca6a8ecd9c1761db3f28058108a0622
Warren Fletcher <warren@fletch.band>
2022-02-07 18:26:33 +0100

    Recursive Implementation of Acerkamns

---
+++ python/ackermans.py
@@ -0,0 +1,20 @@
+def ackerman(m,n):
+    # Base Case
+    if  m ==  0:
+        return n+1
+    elif n == 0:
+        return ackerman(m-1, 1)
+    else:
+        return ackerman(m-1, ackerman(m,n-1))
+
+
+if __name__ == "__main__":
+    print(ackerman(1,1))
+    print(ackerman(2,1))
+    print(ackerman(3,1))
+#    print(ackerman(4,1))
+    print(ackerman(1,2))
+    print(ackerman(2,2))
+    print(ackerman(3,2))
+ #   print(ackerman(4,2))
+
