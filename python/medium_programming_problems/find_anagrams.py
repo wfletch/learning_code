6e4724c430ecf176e723eb2d99729a0df2a8839b
Warren Fletcher <warren@fletch.band>
2022-03-28 16:25:15 +0200

    My Version of Linear Time

--- python/medium_programming_problems/find_anagrams.py
+++ python/medium_programming_problems/find_anagrams.py
@@ -51,29 +51,24 @@ class Solution:
 
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        ns, np = len(s), len(p)
-        if ns < np:
-            return []
-
-        p_count, s_count = [0] * 26, [0] * 26
-        # build reference array using string p
-        for ch in p:
-            p_count[ord(ch) - ord('a')] += 1
-        
-        output = []
-        # sliding window on the string s
-        for i in range(ns):
-            # add one more letter 
-            # on the right side of the window
-            s_count[ord(s[i]) - ord('a')] += 1
-            # remove one letter 
-            # from the left side of the window
-            if i >= np:
-                s_count[ord(s[i - np]) - ord('a')] -= 1
-            # compare array in the sliding window
-            # with the reference array
+        len_s,len_p = len(s), len(p)
+        s_count, p_count = [0] * 26, [0] * 26
+        #Populate initial state
+        for i in p:
+            p_count[ord(i) - ord('a')] +=1
+        # Empty Solutions
+        solutions = []
+        for i in range(len(s)):
+            # Iterate through the word, add letters where appropriate
+            s_count[ord(s[i]) - ord('a')] +=1
+            if i >= len_p:
+                # Maximum lenght of the sliding window has been reached, let's remove
+                # the value at the beginning of the window
+                s_count[ord(s[i - len_p]) - ord('a')] -=1
+            
+            # Check for equality
             if p_count == s_count:
-                output.append(i - np + 1)
+                solutions.append(i - len(p) + 1)
+        return solutions
         
-        return output
         # A solution using just a sliding window and an array
