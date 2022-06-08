131af408282de4d7a1d9910a31a8532aa5b1513f
Warren Fletcher <warren@fletch.band>
2022-06-08 10:49:03 +0200

    Playing around with locks and threads

---
+++ go/goroutines/sleep-cancelled.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"sync"
+	"time"
+)
+
+var mu sync.Mutex
+var done bool
+
+func main() {
+	time.Sleep(1 * time.Second)
+	println("started")
+	go periodic()
+	time.Sleep(5 * time.Second)
+	mu.Lock()
+	done = true
+	mu.Unlock()
+	println("Cancelled")
+	time.Sleep(3 * time.Second)
+}
+
+func periodic() {
+	for {
+		println("tick")
+		time.Sleep(1 * time.Second)
+		mu.Lock()
+		if done {
+			return
+		}
+		mu.Unlock()
+	}
+}
