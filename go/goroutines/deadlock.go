2ad67ac7e2e8d3078cdb58ac3c326fa4765cef9c
Warren Fletcher <warren@fletch.band>
2022-06-08 12:00:37 +0200

    Added comments regarding locking with channels

--- go/goroutines/deadlock.go
+++ go/goroutines/deadlock.go
@@ -1,7 +1,10 @@
 package main
 
 func main() {
-	c := make(chan bool)
+	c := make(chan bool, 20) //Ok, now things get a little interesting
 	c <- true
 	<-c
+	// So, buffered channels.
+	// We can add content but we will only block once the capacity of the buffer has been reached,
+	// Interesting use case. But it would make sense for aggregation and forwarding.
 }
